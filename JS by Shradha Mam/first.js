c637f7111e451e60c40c6d4dd52b3d431ee643ee
kunalarya873 <kunalarya873@gmail.com>
2023-11-18 01:55:34 +0530

    Started JS from Shradha Mam

---
+++ JS by Shradha Mam/first.js
@@ -0,0 +1,11 @@
+console.log("Hello Kunal How are you ???????")
+
+// Practise Question
+
+const product= {
+    name: "Parker Jotter Standard CT Ball Pen",
+    rating: 3.5,
+    price: 270,
+    offer: 5,
+};
+
