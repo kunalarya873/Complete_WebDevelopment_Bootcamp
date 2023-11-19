ce02b0a5c46d79a38ab5edec44236c4915b4ca2e
kunalarya873 <kunalarya873@gmail.com>
2023-11-19 23:27:48 +0530

    Todays topics are String and Loops etc

--- JS by Shradha Mam/first.js
+++ JS by Shradha Mam/first.js
@@ -24,21 +24,48 @@ Unary Operators
 
 // let number= prompt("Enter a number");
 // if(number % 5 === 0){
-//     console.log("Multiple of 5")
+//     sssonsole.log("Multiple of 5")
 // }else{
 //     console.log("Not Multiple of 5");
 // }
+// let marks;
+// marks = Number(prompt('Please enter your Marks'));
+// if (marks >= 80 && marks <= 100) {
+//     alert('Your grade is A');
+//     } else if (marks > 69 && marks < 80) {
+//         alert('Your Grade is B');
+//         } else if (marks > 59 && marks < 70) {
+//             alert('Your Grade is C');
+//             } else if (marks > 49 && marks < 60) {
+//                 alert('Your Grade is D');
+//                 } else {
+//                     alert('You Failed!')
+//                 }
 
-let marks;
-marks = Number(prompt('Please enter your Marks'));
-if (marks >= 80 && marks <= 100) {
-    alert('Your grade is A');
-    } else if (marks > 69 && marks < 80) {
-        alert('Your Grade is B');
-        } else if (marks > 59 && marks < 70) {
-            alert('Your Grade is C');
-            } else if (marks > 49 && marks < 60) {
-                alert('Your Grade is D');
-                } else {
-                    alert('You Failed!')
-                }
+
+// for(let i=0; i<=100; i++){
+//     if(i%2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+
+//Template Literals
+
+/*let SpecialString = `Kunal Arya`;
+console.log(SpecialString);
+
+
+let obj = {
+    item:"Pen",
+    price: 100,
+}
+console.log("Object is ",obj.item,  "and its price is ", obj.price, "rupees");
+
+*/
+
+
+let name = prompt("Enter you name");
+name = name.trim()
+alert(`Your Username is ${name+ '@gmail.com'}`)
