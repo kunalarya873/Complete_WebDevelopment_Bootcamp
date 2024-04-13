118ca9e6ae33214811629405e784751ded79e30f
kunalarya873 <kunalarya873@gmail.com>
2024-04-13 13:31:23 +0530

    Created custom React for understanding concepts

--- React-with-Hitesh/01basicApp/src/main.jsx
+++ React-with-Hitesh/01basicApp/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+const variable = 'Variable'
+const reactElementA = (
+  <a href='https://google.com' target='_blank'>Google1{variable}</a>
+)
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com' },
+  'Google2'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    reactElementA
 )
