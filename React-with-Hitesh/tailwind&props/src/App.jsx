e9609df29e984b46068af6d4d601566ccf4b8000
kunalarya873 <kunalarya873@gmail.com>
2024-04-13 15:21:41 +0530

    Adding Tailwind and props

---
+++ React-with-Hitesh/tailwind&props/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/cards.jsx'
+function App() {
+  // let myObj = {
+  //   username: 'hitesh',
+  //   age: 20,
+  // }
+  // let newArr = [1, 2,3]
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+    <Card username='chaiwithcode' btnText="clickMe"/>
+    <Card username='bijli' btnText="visitMe"/>
+    </>
+  )
+}
+
+export default App
