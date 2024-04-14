106188a1f4bc11bdcc37e806770781e98a444a1f
kunalarya873 <kunalarya873@gmail.com>
2024-04-14 11:07:04 +0530

    Adding the full app but api is not working

--- React-with-Hitesh/06currencyConverter/src/hooks/useCurrencyinfo.js
+++ React-with-Hitesh/06currencyConverter/src/hooks/useCurrencyinfo.js
@@ -1,17 +1,14 @@
-import { useState, useEffect } from "react";
-
-function useCurrencyinfo(currency){
-    const [data, setData] = useState({});
-    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-
-    useEffect(() =>{
-        fetch(url)
-        .then((res)=>res.json())
-        .then((res) => setData(res[currency])) 
-        console.log(data)
+import {useEffect, useState} from "react"
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/${currency}/eur.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
     }, [currency])
-    console.log(data)
+    console.log(data);
     return data
 }
 
-export default useCurrencyinfo;
+export default useCurrencyInfo;
