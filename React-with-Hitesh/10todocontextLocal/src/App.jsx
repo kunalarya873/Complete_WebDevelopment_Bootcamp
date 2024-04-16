735f0cc3a302e30039c6df10574ed5b2cbf07fd6
kunalarya873 <kunalarya873@gmail.com>
2024-04-16 13:27:01 +0530

    Todo app is still ongoing

---
+++ React-with-Hitesh/10todocontextLocal/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className=' text-center text-3xl text-blue-500 flex justify-center align-middle m-4 p-4'>
+      Hello Vite + React!
+    </h1>
+    </>
+  )
+}
+
+export default App
