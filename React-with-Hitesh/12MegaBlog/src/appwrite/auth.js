22193c6e60889fcf7368b0d7f01f56a2921d7b4a
kunalarya873 <kunalarya873@gmail.com>
2024-04-21 23:36:19 +0530

    just getting started

--- React-with-Hitesh/12MegaBlog/src/appwrite/auth.js
+++ React-with-Hitesh/12MegaBlog/src/appwrite/auth.js
@@ -1,6 +1,5 @@
 import config from  '../config/config';
 export class AuthService{
-    client = new ClientEncryption()
 }
-const authService = new AuthService():
+const authService = new AuthService();
 export default AuthService
