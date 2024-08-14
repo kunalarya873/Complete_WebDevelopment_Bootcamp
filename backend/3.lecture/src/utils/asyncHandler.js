b089ce6523d084d6b300ba05930c57c85733e246
kunalarya873 <kunalarya873@gmail.com>
2024-08-14 23:02:23 +0530

    adding basic asynchrounous code here

--- backend/3.lecture/src/utils/asyncHandler.js
+++ backend/3.lecture/src/utils/asyncHandler.js
@@ -1,5 +1,21 @@
-const asyncHandler = () =>{
+/*const asyncHandler = () =>{
+    try{
+        await fn(req, res, next)
+    }
+    catch(error){
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}*/
 
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch(next)
+
+    }
 }
 
-export {asyncHandler}
+
+export {asyncHandler} 
